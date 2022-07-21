ea6fce9043df3d61ea8e5482287e3ebab2be42c3
Sakshamag21 <107304333+Sakshamag21@users.noreply.github.com>
2022-07-21 13:34:02 +0530

    dfdsf

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import Game from "../components/Game";
 // import "./index.css";
 //import "antd/dist/antd.css"
+import { Layout } from "antd";
 import {RecoilRoot} from "recoil";
+const {Content}=Layout;
 export default function Home(){
     return(
         <RecoilRoot>
-        <Game></Game></RecoilRoot>
+       <Content style={{backgroundColor:"crimson",width:"100vw",height:"100vh",zIndex:"1000",position:"absolute",top:"0px",left:"0px"}}> <Game></Game></Content>
+       </RecoilRoot>
 
     )
 }
